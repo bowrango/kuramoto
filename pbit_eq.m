69e43a536b4d47c4c14ff97b3c59ec6ef91377a3
matt@gigabug.org <bowrango@gmail.com>
2025-12-15 21:14:40 -0500

    annealed Gibbs sampling with plot

--- pbit_eq.m
+++ pbit_eq.m
@@ -1,5 +1,5 @@
 
-% Compute the stationary distribution of p-bits using 3 methods.
+% Stationary p-bit distribution
 
 % [1] https://arxiv.org/pdf/2302.06457
 % [2] https://arxiv.org/abs/2007.12331
@@ -8,60 +8,161 @@
 
 % See sota [3];
 
-n = 12;
+% TODO What can I say for positive definite Q?
+
+n = 8;
 N = 2^n;
-% beta = 0.1;
-beta = 0.1;
+tau0 = 5;
+tau = @(t) tau0/t;
 
 rng default
 
-W = randn(n);
-W = (W + W')/2;
-h = diag(W);
-W(1:n+1:end)=0;
+% symmetric i.i.d Gaussian Qij ~ N(0, 1)
+Q = randn(n);
+h = diag(Q);
+Q = tril(Q,-1) + triu(Q',1);
 
 % 1. Markov Transition
-T1 = onetrans(W, h, beta);
-T2 = systrans(W, h, beta);
-mc = dtmc(T1);
-[probmc,tMix] = asymptotics(mc);
-probmc = probmc';   
-[ev, lambda] = eigs(T1.', 2);
-probev = ev(:,1) / sum(ev(:,1));
-% gap = 1 - lambda(2);
+% P1 = unitrans(Q, h, beta);
+% mc = dtmc(P1);
+% [probm, tMix] = asymptotics(mc);
+% probm = probm';   
+% [ev, lambda] = eigs(P1.', 2);
+% sgap = 1 - lambda(2);
 % epsilon = 1e-3;
-% tmix = log(1/epsilon) / gap;
-% numGibbsSteps = ceil(n*tmix);
-numGibbsSteps = 1e5;
+% tmix = log(1/epsilon) / sgap;
 
-% 2. Gibbs sampling
+
+T = 20;
+
+% Boltzmann
+% E(k) = -(0.5*X(:,k)'*Q*X(:,k) + h'*X(:,k));
+X = (2*(dec2bin(0:N-1, n)=='1') - 1)';
+Eb = -(sum(X.*(Q*X), 1)/2 + h'*X);
+[Eb, perm] = sort(Eb);
+% expE = exp(-beta.*Eb);
+% probb = expE/sum(expE);
+% probb = probb(perm);
+
+numGibbsSteps = 1e3;
+
 pbits = sign(randn(n, 1));
-counts = zeros(2^n, 1);
-for t = 1:numGibbsSteps
-    % for ii = 1:n % system-scan
-    %     I = W(ii,:)*pbits + h(ii);
-    %     pbits(ii) = sign(tanh(beta*I) - (2*rand-1));
-    % end
-    idx = randi(n); % random-scan
-    I = W(idx,:) * pbits + h(idx);
-    pbits(idx) = sign(tanh(beta * I) - (2*rand-1));
-    bits = (pbits+1)/2;
-    idx = bin2dec(join(string(bits'),""))+1;
-    counts(idx) = counts(idx) + 1;
+Et = zeros(numGibbsSteps,T);
+F = zeros(N, T);
+% counts = zeros(N, 1);
+for t = 1:T
+    beta = 1/tau(t);
+
+    % Markov Transition
+    Pt = unitrans(Q, h, beta);
+    mc = dtmc(Pt);
+    [probm, tMix] = asymptotics(mc);
+    probm = probm';
+    probm = probm(perm);
+    
+    % Gibbs Sampling
+    counts = zeros(N, 1);
+    for k = 1:numGibbsSteps
+        % for ii = 1:n % system-scan
+        %     I = W(ii,:)*pbits + h(ii);
+        %     pbits(ii) = sign(tanh(beta*I) - (2*rand-1));
+        % end
+        idx = randi(n); % random-scan
+    
+        I = Q(idx,:)*pbits + h(idx);
+        pbits(idx) = sign(tanh(beta*I) - (2*rand-1));
+    
+        bits = (pbits+1)/2;
+        idx = bin2dec(join(string(bits'),""))+1;
+        counts(idx) = counts(idx)+1;
+        Et(k,t) = -(0.5*pbits'*Q*pbits + h'*pbits);
+    end
+    probg = counts/sum(counts);
+
+    F(:,t) = probg(perm);
 end
-probg = counts/sum(counts);
+
+% aggregate probability by energy
+[Ebin, ~, eidx] = unique(Eb);
+Fbin = zeros(length(Ebin), T);
+for t = 1:T
+    for k = 1:length(Ebin)
+        Fbin(k,t) = sum(F(eidx==k, t));
+    end
+end
+
+% remove near-zero probability
+minProb = 1/numGibbsSteps;
+mask = any(Fbin >= minProb, 2);
+Eplot = Ebin(mask);
+Fplot = Fbin(mask,:);
+
+[Tgrid, Egrid] = meshgrid(1:T, Eplot);
+mask = Fplot >= minProb;
+scatter(Tgrid(mask), Egrid(mask), 200, Fplot(mask), 'filled');
+colormap(sky)
+cbar = colorbar; 
+ylabel(cbar,'Probability')
+hold on
+
+% TODO Misleading because +- std can go below the min(Eb)
+% Emean = mean(Et);
+% Estd = std(Et);
+% errorbar(1:T, Emean, Estd, 'k.', 'LineWidth',1.5, 'MarkerSize',10);
+
+yline(min(Eb))
+
+xlabel('Time')
+ylabel('Energy')
+grid on
+
+
+% hdl = imagesc(1:T, Eplot, Fplot);
+% xticks(1:T)
+% set(gca,'YDir','normal')
+% colorbar
+% xlabel('t')
+% ylabel('Energy')
+
+% hdl = bar3(Eplot, Fplot, 'blue');
+% xlabel('t')
+% ylabel('Energy')
+% zlabel('Probability')
+
+% for k = 1:numel(hdl)
+%     Z = hdl(k).ZData;
+%     mask = Z < minProb;
+%     hdl(k).AlphaData = ~mask;
+%     hdl(k).FaceAlpha = 'flat';
+%     hdl(k).EdgeColor = 'none';
+% end
 
 % Only for large gibbs steps
 % TV = 0.5 * sum(abs(probg - probmc));
 % assert(TV < epsilon);
 
-% 3. Boltzmann
-states = (2*(dec2bin(0:N-1, n)=='1') - 1)';
-E = (sum(states.*(W*states), 1)/2 + h'*states)';
-expE = exp(-beta.*E);
-probb = expE/sum(expE);
+% Kmax = 50;                 
+% tv = zeros(Kmax,1);
+% % worst-case initial state
+% mu = zeros(N,1);
+% mu(1) = 1;
+% % tv(k) decays like exp(-k/tmix)
+% for k = 1:Kmax
+%     tv(k) = 0.5 * sum(abs(mu - probm));
+%     mu = T1.'*mu;
+% end
+% figure;
+% semilogy(1:Kmax, tv, 'o-');
+% grid on;
+% xlabel('Single-spin updates (steps of T1)');
+% ylabel('Total variation distance');
 
-% figure
+% [Eb, perm] = sort(Eb);
+% probg = probg(perm);
+% probb = probb(perm);
+% probm = probm(perm);
+
+% figurex
 % spy(T)
 % title('P(x->y)')
 % label = 1:N;
@@ -71,34 +172,61 @@ probb = expE/sum(expE);
 % yticklabels(dec2bin(label-1))
 
 % TODO yyaxis quality
-figure
-grid on
-hold on
-plot(sort(probg), 'bx')
-plot(sort(probb), 'bo')
-% plot(sort(probev), 'g-')
-plot(sort(probmc), 'r-')
-xlim([0 N])
-xlabel('State')
-ylabel('Probability')
-legend('Gibbs', 'Boltzmann', 'dtmc', Location='best')
+% figure
+% grid on
+% hold on
+% plot(Eb, probg, 'bx')
+% plot(Eb, probb, 'ro')
+% plot(Eb, probm, 'r-')
+% xlabel('Energy H(x)')
+% ylabel('Probability')
+% legend('Gibbs', 'Boltzmann', 'Markov', Location='best')
 
-function P = onetrans(W, h, beta)
-% Transition matrix for one uniform random p-bit update (random scan)
+
+% Compare to SK theory
+
+% sigma2 = sum(triu(Q,1).^2, 'all') + sum(h.^2);
+% 
+% betas = linspace(0, 1.0, 40);
+% E_emp = zeros(size(betas));     
+% E_the = zeros(size(betas));    
+% for ii = 1:length(betas)
+%     beta = betas(ii);
+% 
+%     w = exp(-beta*E);
+%     w = w/sum(w);
+% 
+%     E_emp(ii) = sum(w .* E);
+%     E_the(ii) = -beta * sigma2;
+% end
+% 
+% figure; hold on; grid on;
+% plot(betas, E_emp/n, 'bo-', 'LineWidth', 1.2, 'DisplayName', 'Empirical');
+% plot(betas, E_the/n, 'r--', 'LineWidth', 1.5, 'DisplayName', 'SK Theory');
+% 
+% xlabel('\beta');
+% ylabel('Average Spin Energy <E>/n');
+% legend('Location','best');
+
+
+function P = unitrans(Q, h, beta)
+% Transition matrix for a uniform random p-bit update (random scan)
 % P(i,j) is the probability state i transitions to state j.
-% Rows sum to 1. Zeros are on impossible states.
-n = size(W,1);
+% Rows sum to 1. States with Hamming distance more than 1 have no
+% probability.
+n = size(Q,1);
 N = 2^n;
-states = 2*(dec2bin(0:N-1, n)=='1') - 1;
+X = 2*(dec2bin(0:N-1, n)=='1') - 1;
 P = zeros(N);
 for ii = 1:N
-    m1 = states(ii, :);
+    x = X(ii, :);
     for idx = 1:n
         jj = bitxor(ii-1, bitshift(1, n-idx)) + 1;
-        m2 = m1;
-        m2(idx) = -m1(idx);
-        f = W(idx,:)*m2' + h(idx);
-        p = (m2(idx)*tanh(beta*f) + 1) / 2;
+
+        f = (Q(idx,:)*x' + h(idx));
+        % p = (1 - x(idx)*tanh(beta*f))/2;    
+        p = 1 / (1 + exp(2*beta*x(idx)*f));
+        
         P(ii,jj) = P(ii,jj) + p/n;
         P(ii,ii) = P(ii,ii) + (1-p)/n;
     end
@@ -109,6 +237,7 @@ function P = systrans(W, h, beta)
 % Transition matrix for an all p-bit update (system scan)
 % P(i,j) is the probability state i transitions to state j.
 % Rows sum to 1. All elements are non-zero.
+% FIXME negative eigs
 n = size(W, 1);
 N = 2^n;
 states = 2*(dec2bin(0:N-1, n)=='1') - 1;
@@ -118,7 +247,7 @@ for i = 1:N
     f = W*s + h;
     for j = 1:N
         sp = states(j, :)';
-        p = 0.5 * (1 + sp .* tanh(beta * f));
+        p = 0.5 * (1 + sp .* tanh(beta*f));
         P(i,j) = prod(p);
     end
 end
